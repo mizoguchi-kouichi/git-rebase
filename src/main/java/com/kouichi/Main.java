2dc362d4b172ea734c95691391898a01c61b399c
”溝口光一” <guangyigoukou7@gmail.com>
2024-08-05 16:21:29 +0900

    おはようございます！の出力

---
+++ src/main/java/com/kouichi/Main.java
@@ -0,0 +1,8 @@
+package com.kouichi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.printf("おはようございます！");
+    }
+}
